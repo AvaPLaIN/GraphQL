1d857b2cf02e6f04638580540e4ed465135e1f0b
AvaSama <kevin.voss99@gmail.com>
2021-12-08 19:10:53 +0100

    .

--- graphql/index.js
+++ graphql/index.js
@@ -1,13 +1,7 @@
 const { ApolloServer } = require('apollo-server');
-const { makeExecutableSchema } = require('@graphql-tools/schema');
 
-const { animeType, userType, query } = require('./typedefs/index');
+const { typeDefs } = require('./typedefs/index');
 const { resolvers } = require('./resolvers/index');
-
-const typeDefs = makeExecutableSchema({
-  typeDefs: [query, userType, animeType],
-  resolvers,
-});
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
--- graphql/typedefs/index.js
+++ graphql/typedefs/index.js
@@ -2,4 +2,6 @@ const { animeType } = require('./types/anime');
 const { userType } = require('./types/user');
 const { query } = require('./query');
 
-module.exports = { animeType, userType, query };
+const typeDefs = [query, userType, animeType];
+
+module.exports = { typeDefs };
