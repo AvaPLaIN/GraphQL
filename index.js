14d4d2df387d05cc9f1f429cecbd240273694e10
AvaSama <kevin.voss99@gmail.com>
2021-12-08 20:00:37 +0100

    .

--- index.js
+++ index.js
@@ -5,14 +5,10 @@ const connectDB = require('./config/database');
 
 //     * SERVER
 const server = require('./graphql/index');
-// const { ApolloServer } = require('apollo-server');
 
 //     * GRAPHQL
-// const gql = require('graphql-tag');
 
 //     * MODELS
-const Anime = require('./models/Anime');
-const User = require('./models/User');
 
 //     * MIDDLEWARE
 
@@ -20,44 +16,6 @@ const User = require('./models/User');
 
 connectDB();
 const PORT = process.env.PORT || 8800;
-
-//! GraphQL TYPE
-// const typeDefs = gql`
-//   type Anime {
-//     _id: ID!
-//     title: String!
-//     description: String!
-//     banner: String!
-//     thumnail: String!
-//     logo: String!
-//     trailer: String!
-//     genres: [String]!
-//     status: String!
-//     released: String!
-//     episodes: [String]!
-//   }
-//   type Query {
-//     getAnimes: [Anime]
-//   }
-// `;
-
-// const resolvers = {
-//   Query: {
-//     getAnimes: async () => {
-//       try {
-//         const animes = await Anime.find({}, null, { limit: 50 });
-//         return animes;
-//       } catch (error) {
-//         throw new Error(error);
-//       }
-//     },
-//   },
-// };
-
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-// });
 
 server
   .listen({ port: PORT })
